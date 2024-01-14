d5fac10080de4d63bdeb7bd4f22b1fce200ed265
Your NameIvanShtyrov <shtyrov.ivan@gmail.com>
2024-01-14 22:50:10 +0300

    корректировки

--- src/main/scala/PrimeGenerator.scala
+++ src/main/scala/PrimeGenerator.scala
@@ -1,39 +1,17 @@
 object PrimeGenerator {
 
-  /**
-    * Проверяет, является ли число простым.
-    *
-    * @param n Проверяемое число.
-    * @return true, если число простое, иначе false.
-    */
   def isPrime(n: Int): Boolean = {
     if (n <= 1) false
     else if (n == 2) true
-    else if (n % 2 == 0) false
-    else {
-      val sqrtN = Math.sqrt(n).toInt + 1
-      !(3 to sqrtN by 2).exists(i => n % i == 0)
-    }
+    else !(2 until Math.sqrt(n).toInt + 1).exists(n % _ == 0)
   }
 
-  /**
-    * Проверяет, содержит ли число уникальные цифры.
-    *
-    * @param n Проверяемое число.
-    * @return true, если все цифры уникальны, иначе false.
-    */
-  def hasUniqueDigits(n: Int): Boolean = {
-    val individualDigits = n.toString.map(_.asDigit)
-    individualDigits.distinct.length == individualDigits.length
+  def hasAscendingDigits(n: Int): Boolean = {
+    val digits = n.toString.map(_.asDigit)
+    digits.length == 1 || digits.sliding(2).forall { case Seq(a, b) => a < b }
   }
 
-  /**
-    * Генерирует простые числа с уникальными цифрами до заданного предела.
-    *
-    * @param upperLimit Предел для генерации простых чисел.
-    * @return Последовательность простых чисел с уникальными цифрами.
-    */
-  def generatePrimesWithUniqueDigits(upperLimit: Int): Seq[Int] = {
-    (2 to upperLimit).filter(i => isPrime(i) && hasUniqueDigits(i))
+  def generateAscendingPrimes(upperLimit: Int): Seq[Int] = {
+    (2 to upperLimit).filter(i => isPrime(i) && hasAscendingDigits(i))
   }
 }
--- test/scala/PrimeGeneratorTest.scala
+++ test/scala/PrimeGeneratorTest.scala
@@ -1,6 +1,5 @@
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import scala.util.Random
 
 class PrimeGeneratorTest extends AnyFunSuite with Matchers {
 
@@ -12,55 +11,18 @@ class PrimeGeneratorTest extends AnyFunSuite with Matchers {
     PrimeGenerator.isPrime(4) should be(false)
   }
 
-  test("hasUniqueDigits should return true for a number with unique digits") {
-    PrimeGenerator.hasUniqueDigits(123) should be(true)
+  test("hasAscendingDigits should return true for a number with ascending digits") {
+    PrimeGenerator.hasAscendingDigits(123) should be(true)
   }
 
-  test("hasUniqueDigits should return false for a number with repeated digits") {
-    PrimeGenerator.hasUniqueDigits(113) should be(false)
+  test("hasAscendingDigits should return false for a number without ascending digits") {
+    PrimeGenerator.hasAscendingDigits(321) should be(false)
   }
-
-  test("generatePrimesWithUniqueDigits should generate correct primes") {
-    val primes = PrimeGenerator.generatePrimesWithUniqueDigits(10)
-    primes should contain theSameElementsAs Seq(2, 3, 5, 7)
-  }
-
-  test("isPrime should return false for negative numbers") {
-    PrimeGenerator.isPrime(-1) should be(false)
-    PrimeGenerator.isPrime(-10) should be(false)
-    PrimeGenerator.isPrime(Int.MinValue) should be(false)
-  }
-
-  test("isPrime should return false for zero") {
-    PrimeGenerator.isPrime(0) should be(false)
-  }
-
-  test("generatePrimesWithUniqueDigits should not return primes for negative upper limit") {
-    PrimeGenerator.generatePrimesWithUniqueDigits(-1) should be(empty)
-    PrimeGenerator.generatePrimesWithUniqueDigits(-100) should be(empty)
-  }
-
-  test("generatePrimesWithUniqueDigits should not return primes for upper limit of zero") {
-    PrimeGenerator.generatePrimesWithUniqueDigits(0) should be(empty)
-  }
-
-  // Дополнительные тесты
-  test("generatePrimesWithUniqueDigits should return only primes with unique digits") {
-    val primes = PrimeGenerator.generatePrimesWithUniqueDigits(100)
-    primes.foreach { prime =>
-      prime.toString.toSet.size shouldBe prime.toString.length
-    }
-  }
-
-  test("generatePrimesWithUniqueDigits should include all primes up to a small limit") {
-    val smallLimit = 50
-    val expectedPrimes = Seq(2, 3, 5, 7, 13, 23, 31, 37, 43, 47)
-    val primes = PrimeGenerator.generatePrimesWithUniqueDigits(smallLimit)
-    primes should contain theSameElementsAs expectedPrimes
-  }
-
-  test("generatePrimesWithUniqueDigits should handle random upper limits correctly") {
-    val randomUpperLimit = Random.nextInt(100) + 2 // Гарантируем, что предел больше 1
-    noException should be thrownBy PrimeGenerator.generatePrimesWithUniqueDigits(randomUpperLimit)
+  test("generateAscendingPrimes should generate correct ascending primes") {
+    val upperLimit = 100
+    val primes = PrimeGenerator.generateAscendingPrimes(upperLimit)
+    val expectedPrimes = Seq
+    (2, 3, 5, 7, 13, 17, 19, 23, 29, 37, 47, 59, 67, 79, 89)
   }
 }
+
